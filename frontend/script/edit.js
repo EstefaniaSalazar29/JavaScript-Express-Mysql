daf50d69c6f1d88ab791109ef3ca6945bd006b1b
Estefania Salazar Valencia <estefaniasalazarv@iesanagustin.edu.co>
2022-04-17 23:10:18 -0500

    Sprint1

--- frontend/script/edit.js
+++ frontend/script/edit.js
@@ -1,10 +1,4 @@
 const tabla = document.querySelector("#datos-vehiculo tbody");
-const modal = document.getElementById('exampleModal');
-let marca = document.getElementById('marca');
-let linea = document.getElementById('linea');
-let color = document.getElementById('color');
-let placa = document.getElementById('placa');
-let form = document.getElementById('modalform');
 
 
 function cargarU() {
@@ -18,8 +12,6 @@ function cargarU() {
             <td>${vehiculo.linea}</td>
             <td>${vehiculo.color}</td>
             <td>${vehiculo.placa}</td>
-            <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever>Editar</button></td>
-            <td><button type="submit" class="btn btn-danger d-block mx-auto" id="v_${vehiculo.placa}">Borrar</button></td>
             `;
         tabla.appendChild(row);
       });
@@ -27,45 +19,12 @@ function cargarU() {
     .catch((error) => console.log("Hubo un error"));
 }
 cargarU();
-let body = document.getElementById('body')
-body.addEventListener('click', (e)=> {
-    let valor = e.target.id
-    console.log(valor);
-})
 
 
-// let fila;
-// modal.addEventListener('click', (e)=> {
-//   let targe = e.target.id
-//   fila = document.getElementsByTagName("td");
-//   marca= fila[0];
-//   linea= fila[1];
-//   color= fila[2];
-//   placa= fila[3];
-//   console.log(linea);
-//   form.innerHTML = `<form>
-//   <div class="mb-3">
-//     <label id="marca" class="col-form-label">Marca</label>
-//     <input type="text" class="form-control" id="nombreM" value="${marca}">
-//   </div>
-//   <div class="mb-3">
-//     <label id="linea" class="col-form-label">Linea</label>
-//     <input type="text" class="form-control" id="Nomb" value="${linea}">
-//   </div>
-//   <div class="mb-3">
-//     <label id="color" class="col-form-label">Color</label>
-//     <input type="text" class="form-control" id="recipient-name" value="${color}">
-//   </div>
-//   <div class="mb-3">
-//     <label id="Placa" class="col-form-label">Placa</label>
-//     <input type="text" class="form-control" id="recipient-name" value="${placa}">
-//   </div>
-// </form>`
-// })
 
 
-borrar.onclick = function(){
-  alert('hola')
-}
+
+
+
 
 
